69d33d3abd4e3d259aa534779f56c8a8438e4ed5
read0more <read0more@gmail.com>
2023-05-08 10:08:59 +0900

    :white_check_mark: protectedProcedure

---
+++ src/server/__tests__/trpc.test.ts
@@ -0,0 +1,29 @@
+import { TRPCError } from '@trpc/server';
+import { createContextInner } from '../routers/context';
+import { protectedProcedure, router } from '../trpc';
+
+describe('protectedProcedure', () => {
+  it('throw TRPCError when token is falsy', async () => {
+    const protectedRouter = router({
+      test: protectedProcedure.mutation(() => {}),
+    });
+    const ctx = await createContextInner({
+      token: '',
+      setTokenCookie: () => {},
+    });
+    const caller = protectedRouter.createCaller(ctx);
+    await expect(caller.test()).rejects.toBeInstanceOf(TRPCError);
+  });
+
+  it('works fine with truthy token', async () => {
+    const protectedRouter = router({
+      test: protectedProcedure.mutation(() => {}),
+    });
+    const ctx = await createContextInner({
+      token: 'this_is_token',
+      setTokenCookie: () => {},
+    });
+    const caller = protectedRouter.createCaller(ctx);
+    await expect(caller.test()).resolves.toBeUndefined();
+  });
+});
