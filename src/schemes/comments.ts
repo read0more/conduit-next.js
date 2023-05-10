1c76c8a325974ec94091f1f88c15252ee4dcfb44
read0more <read0more@gmail.com>
2023-05-10 22:03:31 +0900

    :sparkles: get, update and delete comment

--- src/schemes/comments.ts
+++ src/schemes/comments.ts
@@ -9,6 +9,11 @@ export const getScheme = z.object({
   slug: z.string().min(1),
 });
 
-export const updateScheme = createScheme;
+export const updateScheme = z.object({
+  id: z.number(),
+  body: z.string().min(1).max(200),
+});
 
-export const deleteScheme = getScheme;
+export const deleteScheme = z.object({
+  id: z.number(),
+});
--- src/server/routers/comments.ts
+++ src/server/routers/comments.ts
@@ -1,4 +1,9 @@
-import { createScheme, getScheme } from 'src/schemes/comments';
+import {
+  createScheme,
+  deleteScheme,
+  getScheme,
+  updateScheme,
+} from 'src/schemes/comments';
 import { procedure, protectedProcedure, router } from '../trpc';
 import prisma from 'src/lib/server/prismaClient';
 
@@ -8,15 +13,11 @@ const create = protectedProcedure
     const { slug, body } = input;
     const { user } = ctx;
 
-    const article = await prisma.article.findUnique({
+    const article = await prisma.article.findUniqueOrThrow({
       where: {
         slug,
       },
     });
-
-    if (!article) {
-      throw new Error('Article not found');
-    }
 
     const comment = await prisma.comment.create({
       data: {
@@ -39,8 +40,78 @@ const create = protectedProcedure
     };
   });
 
+const get = procedure.input(getScheme).query(async ({ input }) => {
+  const { slug } = input;
+
+  const comments = await prisma.comment.findMany({
+    where: {
+      article: {
+        slug,
+      },
+    },
+  });
+
+  return {
+    comments,
+  };
+});
+
+const update = protectedProcedure
+  .input(updateScheme)
+  .mutation(async ({ input, ctx }) => {
+    const { id, body } = input;
+    const { user } = ctx;
+
+    const comment = await prisma.comment.findFirstOrThrow({
+      where: {
+        id,
+        authorId: user?.id,
+      },
+    });
+
+    const updatedComment = await prisma.comment.update({
+      where: {
+        id: comment.id,
+      },
+      data: {
+        body,
+      },
+    });
+
+    return {
+      comment: updatedComment,
+    };
+  });
+
+const remove = protectedProcedure
+  .input(deleteScheme)
+  .mutation(async ({ input, ctx }) => {
+    const { id } = input;
+    const { user } = ctx;
+
+    const comment = await prisma.comment.findFirstOrThrow({
+      where: {
+        id,
+        authorId: user?.id,
+      },
+    });
+
+    await prisma.comment.delete({
+      where: {
+        id: comment.id,
+      },
+    });
+
+    return {
+      comment,
+    };
+  });
+
 export const commentsRouter = router({
   create,
+  get,
+  update,
+  remove,
 });
 
 export type CommentsRouter = typeof commentsRouter;
