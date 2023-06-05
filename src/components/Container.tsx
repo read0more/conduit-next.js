d623a28cd377ce3ebf4d7ed08b1983ba27efc5b3
read0more <read0more@gmail.com>
2023-06-05 18:50:58 +0900

    :lipstick: fix media query

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,15 +4,15 @@ export default styled.div`
   margin: 0 auto;
   padding: 0 1em;
 
-  @media (min-width: 1200px) {
-    max-width: 1140px;
+  @media only screen and (max-width: 600px) {
+    max-width: 540px;
   }
 
-  @media (min-width: 768px) {
+  @media only screen and (min-width: 768px) {
     max-width: 720px;
   }
 
-  @media (min-width: 544px) {
-    max-width: 720px;
+  @media only screen and (min-width: 1200px) {
+    max-width: 1024px;
   }
 `;
